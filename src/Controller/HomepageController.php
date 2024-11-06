2b8827d7543b525d658a6961c09ff7b4fe81184f
Corentin Nicodème <corentin.nicodeme@telecom-sudparis.eu>
2024-11-06 16:32:35 +0100

    Premium things

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,6 +17,43 @@ class HomepageController extends AbstractController
             'controller_name' => 'HomepageController',
         ]);
     }
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/premium', name: 'app_premium', methods: ['POST', 'GET'])]
+    public function put_premium(): Response
+    {
+        $user = $this->getUser();
+
+        // Vérifiez que l'utilisateur est connecté
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour devenir premium.');
+        }
+
+        // Récupérer les rôles existants
+        $roles = $user->getRoles();
+
+        // Ajouter le rôle 'ROLE_PREMIUM' si non présent
+        if (!in_array('ROLE_PREMIUM', $roles, true)) {
+            $roles[] = 'ROLE_PREMIUM';
+            $this->addFlash('success', 'Vous êtes maintenant premium, veuillez vous reconnecter !');
+            $user->setRoles($roles);
+
+            // Sauvegarder dans la base de données
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_login');
+    }
+
+
+
 
 
 }
