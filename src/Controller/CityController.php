7684d00b7783d32898025c65ae4431fe3793f7bd
Corentin Nicodème <corentin.nicodeme@telecom-sudparis.eu>
2024-11-06 01:52:15 +0100

    Ajout de données (fichiers json)

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -12,23 +12,31 @@ class CityController extends AbstractController
 
     #[Route('/city/{country}/{name}/{latitude}/{longitude}', name: 'app_city_show')]
     public function show(ManagerRegistry $doctrine, $name, $latitude, $longitude, $country): Response {
-        dump($country);
+        $country = $this->findCountry(strtoupper($country));
         return $this->render('city/show.html.twig', [
             'controller_name' => 'CityController',
             'country' => $country,
-            'currency' => $this->findCurrency(strtoupper($country)),
+            'flag' => $this->findSomeData($country, 'json_files/country-by-flag.json', 'flag_base64'),
+            'currency' => $this->findSomeData($country, 'json_files/country-by-currency-name.json', 'currency_name'),
+            'capital' => $this->findSomeData($country, 'json_files/country-by-capital-city.json', 'city'),
+            'continent' => $this->findSomeData($country, 'json_files/country-by-continent.json', 'continent'),
+            'government' => $this->findSomeData($country, 'json_files/country-by-government-type.json', 'government'),
+            'population' => $this->findSomeData($country, 'json_files/country-by-population.json', 'population').' habitants',
+            'density' => $this->findSomeData($country, 'json_files/country-by-population-density.json', 'density').' habitants/km²',
+            'languages' => implode(', ', $this->findSomeData($country, 'json_files/country-by-languages.json', 'languages')),
+            'life_expectancy' => $this->findSomeData($country, 'json_files/country-by-life-expectancy.json', 'expectancy').' ans',
+            'national_dish' => $this->findSomeData($country, 'json_files/country-by-national-dish.json', 'dish'),
+            'religion' => $this->findSomeData($country, 'json_files/country-by-religion.json', 'religion'),
+            'surface' => $this->findSomeData($country, 'json_files/country-by-surface-area.json', 'area').' km²',
+            'annual_temperature' => $this->findSomeData($country, 'json_files/country-by-yearly-average-temperature.json', 'temperature').'°C',
+            'phone_code' => '+'.$this->findSomeData($country, 'json_files/country-by-calling-code.json', 'calling_code'),
             'name' => $this->formatCityName($name),
             'latitude' => $latitude,
             'longitude' => $longitude
         ]);
     }
-
-    function findCurrency($country) {
-        // Charger les fichiers JSON
+    function findCountry($country) {
         $countryByAbbreviation = json_decode(file_get_contents('json_files/country-by-abbreviation.json'), true);
-        $countryByCurrencyName = json_decode(file_get_contents('json_files/country-by-currency-name.json'), true);
-
-        // Trouver le pays correspondant à l'abréviation
         $countryData = null;
         foreach ($countryByAbbreviation as $large_country) {
             if ($large_country['abbreviation'] === $country) { // Vous pouvez ajuster cette logique en fonction de votre entrée
@@ -36,21 +44,30 @@ class CityController extends AbstractController
                 break;
             }
         }
+        return $countryData['country'];
+    }
 
 
-        // Trouver la monnaie associée au pays
-        $currencyData = null;
-        if ($countryData) {
-            foreach ($countryByCurrencyName as $currency) {
-                if ($currency['country'] === $countryData['country']) {
-                    $currencyData = $currency;
-                    break;
-                }
+    function findSomeData($country, $jsonFile, $key) {
+        $data = json_decode(file_get_contents($jsonFile), true);
+        $dataFound = null;
+        foreach ($data as $item) {
+            if ($item['country'] === $country) {
+                $dataFound = $item;
+                break;
             }
         }
-        dump($currencyData);
-        return $currencyData['currency_name'];
+        dump($dataFound);
+        return $dataFound[$key];
     }
+
+
+
+
+
+
+
+
     function formatCityName($cityName) {
         // Remplacer "l-" ou "-l-" par "l'" sans espace, sinon remplace les tirets par des espaces
         $formattedName = preg_replace("/\b-l-\b/", "-l'", $cityName);
